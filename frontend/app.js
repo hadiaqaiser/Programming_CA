c1dff7560cf0528777f8cd4981c60bab66ffade9
hadiaqaiser <20069602@mydbs.ie>
2025-10-30 01:04:06 +0000

    addin foundation shades also in mock list. now have 6 total

--- frontend/app.js
+++ frontend/app.js
@@ -4,3 +4,11 @@ const mockShades = [
   {product_name:"Medora Lipstick", shade_name:"Rose", shade_code:"22", finish:"Glowy", color_family:"Pink", msrp:450},
   {product_name:"Medora Lipstick", shade_name:"Rust", shade_code:"23", finish:"Matte", color_family:"Brown", msrp:450},
 ];
+
+// addin foundation product too, so i can test category later
+mockShades.push(
+  {product_name:"Medora Foundation", shade_name:"Ivory", shade_code:"11", finish:"Matte", color_family:"Light", msrp:1200},
+  {product_name:"Medora Foundation", shade_name:"Honey", shade_code:"14", finish:"Glowy", color_family:"Medium", msrp:1200},
+  {product_name:"Medora Foundation", shade_name:"Cocoa", shade_code:"16", finish:"Glowy", color_family:"Dark", msrp:1200}
+);
+
