2b2e0db259f1e60b53e3bb2ee6f1a8e60e99dd1d
hadiaqaiser <20069602@mydbs.ie>
2025-10-30 01:02:24 +0000

    add few sample shade data (lipstick only for now)

---
+++ frontend/app.js
@@ -0,0 +1,6 @@
+// start small data, just lipstick shades for test
+const mockShades = [
+  {product_name:"Medora Lipstick", shade_name:"Cherry", shade_code:"21", finish:"Matte", color_family:"Red", msrp:450},
+  {product_name:"Medora Lipstick", shade_name:"Rose", shade_code:"22", finish:"Glowy", color_family:"Pink", msrp:450},
+  {product_name:"Medora Lipstick", shade_name:"Rust", shade_code:"23", finish:"Matte", color_family:"Brown", msrp:450},
+];
