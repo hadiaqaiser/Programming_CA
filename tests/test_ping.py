1f183088f7f7adde9fb6879cffef0bec3f576b1b
hadiaqaiser <20069602@mydbs.ie>
2025-11-19 23:50:35 +0000

    test ping route returns correct json

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -2,3 +2,11 @@
 import pytest
 from backend.app import app
 
+# use flask test_client to check response code + message
+def test_ping_route():
+    client = app.test_client()
+    res = client.get("/api/ping")
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["message"] == "medoracare api ok"
